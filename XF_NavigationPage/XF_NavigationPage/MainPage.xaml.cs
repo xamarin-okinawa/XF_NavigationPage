bae294fb234249eff8818164ed6ed6321c850757
smbkrysk14 <smbkrysk141414@gmail.com>
2017-07-02 20:03:52 +0900

    プロジェクト ファイルを追加します。

---
+++ XF_NavigationPage/XF_NavigationPage/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_NavigationPage
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            this.BindingContext = new MainPageViewModel(Navigation);
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NextPage());
+        }
+    }
+}
---
+++ XF_NavigationPage/XF_NavigationPage/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XF_NavigationPage
+{
+    public class MainPageViewModel
+    {
+        private INavigation navigation;
+
+        public MainPageViewModel(INavigation nav)
+        {
+            this.navigation = nav;
+            this.ScreenTracitionCommand = new Command(() => ScreenTracition());
+        }
+
+        public ICommand ScreenTracitionCommand { get; }
+        private void ScreenTracition()
+        {
+            if(this.navigation != null)
+            {
+                navigation.PushAsync(new NextPage());
+            }
+        }
+    }
+}
